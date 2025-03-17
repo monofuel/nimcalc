f58260b9967af97782bbf5ec296a93965f5a1263
monofuel <monofuel@japura.net>
2025-03-17 16:04:24 -0400

    pretty print

--- examples/example.nim
+++ examples/example.nim
@@ -3,10 +3,13 @@ import nimcalc
 let ast = parseExpression("(1/2) + (1/5)")
 # will print "1 / 2 + 1 / 5"
 echo "Original: ", toMathString(ast)
+
+# simplify the expression
 let simplified = simplify(ast)
 
 # will print "7/10"
 echo "Simplified: ", toMathString(simplified)
 
+# convert fractions to floating point decimals
 # will print "0.7"
 echo "Decimal: ", toMathString(toDecimal(simplified))
